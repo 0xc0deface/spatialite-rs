97a1a4eaa0daf2d7f360cdcb66efd72ea41c1c0f
Matthew Fletcher <0xc0deface@gmail.com>
2020-06-16 12:21:19 +1000

    ran cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,23 @@
-use sqlite::{ self, Result, Connection };
+use sqlite::{self, Connection, Result};
 use std::path::Path;
 
-pub fn open<T: AsRef<Path>>(path: T) -> Result<Connection>
-{
+pub fn open<T: AsRef<Path>>(path: T) -> Result<Connection> {
     let connection = sqlite::open(path).unwrap();
-    unsafe { sqlite3_sys::sqlite3_enable_load_extension( connection.as_raw(), 1 ); }
-    connection.execute( "SELECT load_extension('mod_spatialite.so');").unwrap();
-    unsafe { sqlite3_sys::sqlite3_enable_load_extension( connection.as_raw(), 0 ); }
-    Ok( connection )
+    unsafe {
+        sqlite3_sys::sqlite3_enable_load_extension(connection.as_raw(), 1);
+    }
+    connection
+        .execute("SELECT load_extension('mod_spatialite.so');")
+        .unwrap();
+    unsafe {
+        sqlite3_sys::sqlite3_enable_load_extension(connection.as_raw(), 0);
+    }
+    Ok(connection)
 }
 
 #[test]
-fn test_open() -> anyhow::Result<()>
-{
+fn test_open() -> anyhow::Result<()> {
     open("spatialite_rs_test.db")?;
-    std::fs::remove_file( "spatialite_rs_test.db" )?;
+    std::fs::remove_file("spatialite_rs_test.db")?;
     Ok(())
 }
