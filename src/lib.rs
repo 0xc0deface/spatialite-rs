a0b9fb24d7846754a9c866001e52f751274cec67
Matthew Fletcher <0xc0deface@gmail.com>
2021-05-04 18:30:04 +1000

    - Added an extra function to also allow loading other extensions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,27 @@
-use sqlite::{self, Connection, Result, OpenFlags};
+use sqlite::*;
 use std::path::Path;
 
 pub fn open<T: AsRef<Path>>(path: T) -> Result<Connection> {
     let connection = sqlite::Connection::open(path)?;
-    load_spatialite_extension( &connection );
+    load_spatialite_extension(&connection);
     Ok(connection)
 }
 
 pub fn open_with_flags<T: AsRef<Path>>(path: T, flags: OpenFlags) -> Result<Connection> {
     let connection = sqlite::Connection::open_with_flags(&path, flags)?;
-    load_spatialite_extension( &connection );
+    load_spatialite_extension(&connection);
     Ok(connection)
 }
 
-fn load_spatialite_extension( conn: &Connection )
-{
+fn load_spatialite_extension(conn: &Connection) {
+    load_custom_extension(conn, "mod_spatialite.so");
+}
+
+fn load_custom_extension(conn: &Connection, module: &str) {
     unsafe {
         sqlite3_sys::sqlite3_enable_load_extension(conn.as_raw(), 1);
     }
-    conn
-        .execute("SELECT load_extension('mod_spatialite.so');")
+    conn.execute(format!("SELECT load_extension('{}');", module))
         .unwrap();
     unsafe {
         sqlite3_sys::sqlite3_enable_load_extension(conn.as_raw(), 0);
@@ -32,7 +34,7 @@ fn test_open() -> anyhow::Result<()> {
         open("spatialite_rs_test.db")?;
     }
 
-    open_with_flags("spatialite_rs_test.db", OpenFlags::new().set_read_only() )?;
+    open_with_flags("spatialite_rs_test.db", OpenFlags::new().set_read_only())?;
     std::fs::remove_file("spatialite_rs_test.db")?;
     Ok(())
 }
